94049876bef23e60231b57650474a4fa5e2e67a8
emekbib <emekbib@students.kennesaw.edu>
2021-09-29 00:34:57 -0400

    Data Binding Angular Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WarningMessageComponent } from './warning-message/warning-message.component';
+import { SuccessMessageComponent } from './success-message/success-message.component';
+import { SelectorComponent } from './selector/selector.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WarningMessageComponent,
+    SuccessMessageComponent,
+    SelectorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
